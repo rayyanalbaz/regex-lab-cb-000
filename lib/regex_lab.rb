e2d9e8ddadcb32a49022316d773f1c79b0f3c268
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-25 19:46:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,6 +3,9 @@ def starts_with_a_vowel?(word)
     if word.scan(/\w[aeiou]+[a-z]/) != []
       return true
     end
+    if word.scan(/\w[^aeiou]+[a-z]/) != []
+      return false
+    end
 
 end
 
