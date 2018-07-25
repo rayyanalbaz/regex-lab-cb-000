284bb064d1b793874c59d5e8c6d9b018aa483175
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-25 19:31:23 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
+
 def starts_with_a_vowel?(word)
+
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/ \Aun+[a-z]+ing\z /)
 end
 
 def words_five_letters_long(text)
