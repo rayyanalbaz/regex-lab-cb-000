4d603a30a336d837ff60800ebce205691c23d0cf
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-25 19:47:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
     if word.scan(/[aeiou]+[a-z]/) != []
       return true
     end
-    if word.scan(/\w[]+/) != []
+    if word.scan(/\w[]+[]/) != []
       return false
     end
 
