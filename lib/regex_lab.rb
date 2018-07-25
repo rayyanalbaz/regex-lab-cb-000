555248bb4cbf093c9ff357becbb110646064ff62
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-25 22:13:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  return text.scan(/\w {5}/)
+  return text.scan(/[a-zA-Z0-9]{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
