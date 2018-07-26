b3896a3be20993cc41d07240f50fe66024f37125
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-26 00:55:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,7 @@
 def starts_with_a_vowel?(word)
     if word.scan(/^([aeoiu])+\w/) != []
       return true
-    elsif word.scan(/^([^aeiou])/) != []
+    elsif word.scan(/^([^aeiou])+\w/) != []
       return false
     end
 
