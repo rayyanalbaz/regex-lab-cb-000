ab027a0484134839b7ed7a8093df894dadbdc6e0
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-26 00:54:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,7 @@
 def starts_with_a_vowel?(word)
     if word.scan(/^([aeoiu])/) != []
       return true
-    elsif word.scan(//) != []
+    elsif word.scan(/^([^aeiou])/) != []
       return false
     end
 
