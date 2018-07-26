549a50f36d59bc51bdaf45be29dd99823c9f5013
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-26 01:31:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,8 @@
 def starts_with_a_vowel?(word)
   if word.scan(/^([aeoiu]|[AEIOU])\w/) != []
     return true
-  end
+  elsif word.scan(/^([^aeoiu]|[^AEIOU])\w/) != []
+    
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
