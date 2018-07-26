173b57c2c9f7eb24605e15d09687e71806c82c17
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-26 00:43:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -35,7 +35,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(?:\+?|\b)[0-9]{10}\b/)
+  if phone.match(/^(\d{3})(\d{3})(\d{4})$/)
     return true
   else return false
   end
