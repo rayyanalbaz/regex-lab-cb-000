c9e84e5a0df07b28f22f1240320212e0e808a4cf
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-26 00:12:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -35,7 +35,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if word.scan(/^\d+[0-9]+\d$/) != []
+  if word.scan(/^[0-9]$/) != []
     return true
   end
 
