fb8c26b4829c2b6ab7d99109c6886ba5ba362d94
Rayyan Albaz <ra5analbaz@hotmail.com>
2018-07-26 00:28:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -37,6 +37,7 @@ end
 def valid_phone_number?(phone)
   if phone.match(/^[0-9]|\(+[0-9]|[\-()]+[0-9]$|\($/)
     return true
-  end
+  elsif phone.match(/^[^0-9]|^\(+[^0-9]|[^\-()]+[^0-9]$|^\($/)
+    
 
 end
